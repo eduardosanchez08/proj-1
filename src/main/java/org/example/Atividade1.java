3ce2e84c7b7435100184f008588d51c3bf515337
eduardosanchez08 <eduardodosanjosprimavera@gmail.com>
2025-08-30 15:54:47 -0300

    Atividade1

---
+++ src/main/java/org/example/Atividade1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Atividade1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
